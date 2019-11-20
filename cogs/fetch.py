08b28e38a63295faebdab1e641b662df16cffd13
moisesjsanchez <moijsanchez@gmail.com>
2019-11-20 03:46:27 -0800

    finished bot

--- cogs/fetch.py
+++ cogs/fetch.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-import asyncio
-from itertools import cycle
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -12,20 +10,28 @@ client = gspread.authorize(creds)
 sheet = client.open("Anime Movie Showing").sheet1
 data = sheet.get_all_records()
 
+#forming one large string to be sent once
+movies = 'Here are the weeb movies currently being played near us in the next few months!\n ---------------------------------------------------------------------------------------\n'
+repeat_item = []
+for item in data:
+    movies += (f'{item["Movie Title"]} is being aired on {item["Showing Date"]}.\n')
+    #repeat_item = item this for checking for duplicates
+
 class Fetch(commands.Cog):
 
-    def __init__(self, client, status):
+    def __init__(self, client):
         self.client = client
-        self.status = status
- 
+    
+    #check if bot is online at time of deployment
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.client.change_presence(status=discord.Status.idle, activity=discord.Game('Hello there!'))
+        await self.client.change_presence(status=discord.Status.idle, activity=discord.Game('Just chillin drinking boba.'))
         print('Bot is online')
 
+    #manual command to fetch from the current sheet 
     @commands.command()
-    async def playing(self,ctx):
-        await ctx.send('Here are the movies currently known!')        
-
+    async def movie(self,ctx):
+        await ctx.send(movies)
+        
 def setup(client): 
     client.add_cog(Fetch(client))
