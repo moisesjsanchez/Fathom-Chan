57a5a061399e9b36d1b07280176572ce26700f95
Moises Jornales Sanchez <36614348+moisesjsanchez@users.noreply.github.com>
2019-11-24 00:34:41 -0800

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -13,9 +13,6 @@ class Help(commands.Cog):
 
         embed.add_field(name = '.help', value = 'Calls up list of commands that user can perform', inline = False)
         embed.add_field(name = '.movies', value = 'Fetchs current Fathom event anime movies playing', inline = False)
-        embed.add_field(name = '.reminder X', value = 'Sets up a reminder that goes off the day before **X** movie plays', inline = False)
-        embed.add_field(name = '.refresh_on', value = 'Enables bot to automatic display what movies are playing on a monthly basis', inline = False)
-        embed.add_field(name = '.refresh_off', value = 'Disables automatic monthly movie messages', inline = False)
 
         await ctx.send(embed = embed)
     
