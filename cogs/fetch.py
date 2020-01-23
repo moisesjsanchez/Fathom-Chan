5cb232b634e977fc0fe80dd6bd61b7d2f4f42783
moisesjsanchez <moijsanchez@gmail.com>
2020-01-22 17:48:24 -0800

    reset cogs

--- cogs/fetch.py
+++ cogs/fetch.py
@@ -5,16 +5,17 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-#connecting to Google Sheets API
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+# connecting to Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ.get('CREDS')), scope)
 client = gspread.authorize(creds)
 sheet = client.open("Anime Movie Showing").sheet1
 data = sheet.get_all_records()
 
-#forming one large string to be sent once
+# forming one large string to be sent once
 movies = 'Here are the weeb movies currently being played near us in the next few months!\n ---------------------------------------------------------------------------------------'
-repeat_item = {"Movie Title": '', "Showing Data": ''} #initial dictonary value for matching
+repeat_item = {"Movie Title": '', "Showing Data": ''}  # initial dictonary value for matching
 for item in data:
     if repeat_item["Movie Title"] == item["Movie Title"]:
         movies += (f' and {item["Showing Date"]}')
@@ -22,21 +23,23 @@ for item in data:
         movies += (f'\n{item["Movie Title"]} is being aired on {item["Showing Date"]}')
     repeat_item = item
 
+
 class Fetch(commands.Cog):
 
-    def __init__(self,client):
+    def __init__(self, client):
         self.client = client
-    
-    #check if bot is online at time of deployment
+
+    # check if bot is online at time of deployment
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.client.change_presence(status=discord.Status.idle, activity=discord.Game('Just chillin drinking boba.'))
+        await self.client.change_presence(status=discord.Status.online, activity=discord.Game('For help use .help command mang'))
         print('Bot is online')
 
-    #manual command to fetch from the current sheet 
+    # manual command to fetch from the current sheet
     @commands.command()
-    async def movies(self,ctx):
+    async def movies(self, ctx):
         await ctx.send(movies)
-        
-def setup(client): 
+
+
+def setup(client):
     client.add_cog(Fetch(client))
--- cogs/help.py
+++ cogs/help.py
@@ -1,20 +1,25 @@
 import discord
 from discord.ext import commands
 
+
 class Help(commands.Cog):
-    
+
     def __init__(self, client):
         self.client = client
 
-    #settings up the custom help functions 
+    # settings up the custom help functions
     @commands.command()
     async def help(self, ctx):
-        embed = discord.Embed(title = 'Fathom Chan', description = "A bot for your Fathom anime film related needs. Below are a list of commands:", color = 0xE69138)
+        embed = discord.Embed(
+            title='Fathom Chan', description="A bot for your Fathom anime film related needs. Below are a list of commands:", color=0xE69138)
 
-        embed.add_field(name = '.help', value = 'Calls up list of commands that user can perform', inline = False)
-        embed.add_field(name = '.movies', value = 'Fetchs current Fathom event anime movies playing', inline = False)
+        embed.add_field(
+            name='.help', value='Calls up list of commands that user can perform', inline=False)
+        embed.add_field(
+            name='.movies', value='Fetchs current Fathom event anime movies playing', inline=False)
 
-        await ctx.send(embed = embed)
-    
+        await ctx.send(embed=embed)
+
+
 def setup(client):
     client.add_cog(Help(client))
