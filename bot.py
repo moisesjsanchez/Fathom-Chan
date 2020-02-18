93d83969f39221ff8621c36fa0d495793028da23
moisesjsanchez <moijsanchez@gmail.com>
2020-02-17 23:55:15 -0800

    Improved PEP8 styling

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import discord
 from discord.ext import commands
 import os
 
-#initalizition 
-client = commands.Bot(command_prefix = '.')
+# initalizition
+client = commands.Bot(command_prefix='.')
 client.remove_command('help')
+
 
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+
 
 @client.command()
 async def unload(ctx, extension):
@@ -16,6 +18,6 @@ async def unload(ctx, extension):
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}') #ensure extension does not load .py.py
+        client.load_extension(f'cogs.{filename[:-3]}')  # ensure extension does not load .py.py
 
-client.run(os.environ['TOKEN']) 
+client.run(os.environ['TOKEN'])
